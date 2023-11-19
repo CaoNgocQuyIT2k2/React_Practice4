85994c30a2cf2e6cac769b032b7b0fa48015ae39
CaoNgocQuyIT2k2 <caongocquyit2k2@gmail.com>
2023-11-19 15:19:47 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import DatVe from './layout/DatVe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DatVe />
     </div>
   );
 }
 
 export default App;
+
